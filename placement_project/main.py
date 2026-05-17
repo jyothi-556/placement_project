8aca9f800888b58b4ee38cc271591f477ae84178
jyothi-556 <jyothilakkireddy56@gmail.com>
2026-05-17 18:48:12 +0530

    Fixed model path

--- placement_project/main.py
+++ placement_project/main.py
@@ -45,8 +45,11 @@ db = SQLAlchemy(app)
 # LOAD ML MODEL
 # =========================================
 
-model = joblib.load("model.pkl")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+model_path = os.path.join(BASE_DIR, "model.pkl")
+
+model = joblib.load(model_path)
 # =========================================
 # GLOBAL REPORT DATA
 # =========================================
